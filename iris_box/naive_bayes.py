267638bd116da297741f3f3286445a9d385c2a94
Jason <jxieeducation@berkeley.edu>
2016-01-05 22:47:33 -0800

    added basic iris box

---
+++ iris_box/naive_bayes.py
@@ -0,0 +1,6 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+print (iris.target != y_pred).sum()
