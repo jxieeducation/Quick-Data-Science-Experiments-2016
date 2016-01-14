dc55c4377e8f238069ac31b58c168a4bd84764cb
Jason <jxieeducation@berkeley.edu>
2016-01-13 23:28:34 -0800

    added scatter plot matrix color

--- iris_box/scatter_plot_matrices.R
+++ iris_box/scatter_plot_matrices.R
@@ -7,4 +7,10 @@ data(iris)
 cor(iris[,1:4]) # covariance is a measure of how one variable is associated with another
 
 ########## method 2: scatter plot matrix
-pairs(iris[,1:4])
+cols <- character(nrow(iris))
+cols[] <- "black"
+
+cols[iris$Species == "versicolor"] <- "blue"
+cols[iris$Species == "setosa"] <- "green"
+cols[iris$Species == "virginica"] <- "red"
+pairs(iris[,1:4],col=cols, labels=c("s. length", "s. width", "c. length", "c. width"))
