da611a1210970c7a4d8d7a35039ae8ab41922506
jx <jxieeducation@berkeley.edu>
2016-10-28 10:55:35 -0700

    added tsfresh examples

---
+++ tsfresh_examples/intro.py
@@ -0,0 +1,8 @@
+import tsfresh.examples
+tsfresh.examples.robot_execution_failures.download_robot_execution_failures()
+
+from tsfresh.examples import load_robot_execution_failures
+
+timeseries, y = load_robot_execution_failures()
+
+
